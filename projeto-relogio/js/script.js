7ed2a2e8b741381dbdd048c273abb455ea0cc5da
florianopolismarcelo <florianopolismarcelo@msn.com>
2023-11-09 05:19:42 -0300

    projeto relogio

--- projeto-relogio/js/script.js
+++ projeto-relogio/js/script.js
@@ -1,13 +1,19 @@
-const hourHand = document.querySelector('.hour-hand');
+const hourHand = document.querySelector(".hour-hand")
 const minuteHand = document.querySelector(".minute-hand")
 const secondHand = document.querySelector(".second-hand")
 
 const getTime = () => {
   const date = new Date()
-  
+
   return {
     hours: date.getHours(),
     minutes: date.getMinutes(),
     seconds: date.getUTCSeconds(),
   }
 }
+setInterval(() => {
+  const { seconds, minutes, hours } = getTime()
+  secondHand.style.transform = `translate(0, -50%) rotate(${seconds * 6}deg)`
+  minuteHand.style.transform = `translate(0, -50%) rotate(${minutes * 6}deg)`
+  hourdHand.style.transform = `translate(0, -50%) rotate(${hours * 30}deg)`
+}, 1000)
