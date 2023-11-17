a0ba1ddfc65c2f8e7cb0f9cba3875602e76fbd8a
florianopolismarcelo <florianopolismarcelo@msn.com>
2023-11-17 11:45:11 -0300

    projeto jogo da memoria

--- projeto-jogo-da-memoria/js/game.js
+++ projeto-jogo-da-memoria/js/game.js
@@ -1,6 +1,6 @@
 const grid = document.querySelector(".grid")
 
-const cards = [
+const characters = [
   "beth",
   "jerry",
   "jessica",
@@ -9,24 +9,36 @@ const cards = [
   "pickle-rick",
   "rick",
   "summer",
-  "neeseeks",
+  "meeseeks",
   "scroopy",
 ]
 
 const createElement = (tag, className) => {
-  const element = document.createElement(tag);
-  element.className = className;
-  return element;
+  const element = document.createElement(tag)
+  element.className = className
+  return element
 }
 
-const createCard = () => {
-
-  const card = createElement("div", "card");
+const createCard = (character) => {
+  const card = createElement("div", "card")
   const front = createElement("div", "face front")
   const back = createElement("div", "face back")
+
+  front.style.backgroundImage = `url('../imagens/${character}.png')`
 
   card.appendChild(front)
   card.appendChild(back)
 
-  return card;
+  return card
 }
+
+const loadGame = () => {
+
+  const duplicateCharacters = [...characters, ...characters];
+  duplicateCharacters.forEach((character) => {
+
+    const card = createCard(character)
+    grid.appendChild(card)
+  })
+}
+loadGame()
