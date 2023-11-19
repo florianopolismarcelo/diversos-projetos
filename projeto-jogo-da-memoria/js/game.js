4d3cbce865a58281cc4f8b8fdf44973789f5fcb7
florianopolismarcelo <florianopolismarcelo@msn.com>
2023-11-19 18:26:55 -0300

    projeto jogo da memoria

--- projeto-jogo-da-memoria/js/game.js
+++ projeto-jogo-da-memoria/js/game.js
@@ -22,6 +22,14 @@ const createElement = (tag, className) => {
 let firstCard = ""
 let secondCard = ""
 
+const checkEndGame = () => {
+  const disabledCards = document.querySelectorAll('.disabled-card');
+
+  if (disabledCards.length == 20) {
+    alert('Parabéns! Você conseguiu')
+  }
+}
+
 const checkCards = () => {
   const firstCharacter = firstCard.getAtribute('data-character');
   const secondCharacter = secondCard.getAtribute("data-character");
@@ -33,6 +41,8 @@ const checkCards = () => {
 
      firstCard = ""
      secondCard = ""
+
+     checkEndGame();
 
   } else {
     setTimeout(() => {
