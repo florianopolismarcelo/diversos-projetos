e9c4d04bc1fa12da46960eaa70f0e34e29828e91
florianopolismarcelo <florianopolismarcelo@msn.com>
2023-11-22 05:34:04 -0300

    jogo da memoria

--- jogo-da-memoria02/script.js
+++ jogo-da-memoria02/script.js
@@ -1,10 +1,6 @@
 const grid = document.querySelector(".grid")
 
-const createElement = (tag, className) => {
-  const element = document.createElement(tag)
-  element.className = className
-  return element
-}
+
 const characters = [
   "beth",
   "jerry",
@@ -17,20 +13,28 @@ const characters = [
   "meeseeks",
   "scroopy",
 ]
+const createElement = (tag, className) => {
+  const element = document.createElement(tag)
+  element.className = className
+  return element
+}
 const createCard = (character) => {
   const card = createElement("div", "card")
   const front = createElement("div", "face front")
   const back = createElement("div", "face back")
 
-  front.style.backgroundImage = `url('../imagens/${character}.png')`
-
   card.appendChild(front)
   card.appendChild(back)
 
-  card.addEventListener("click", revealCard)
-  card.setAttribute("data-character", character)
-
   return card
 }
+const loadGame = () => {
+
+  characters.forEach((character) => {
+    const card = createCard()
+    grid.appendChild(card)
+  });
+}
+loadGame();
 
 
