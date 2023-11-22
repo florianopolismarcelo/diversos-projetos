0402187d1338967e6708290bca3a671e47ecb880
florianopolismarcelo <florianopolismarcelo@msn.com>
2023-11-22 12:47:51 -0300

    jogo da memoria

--- jogo-da-memoria02/script.js
+++ jogo-da-memoria02/script.js
@@ -17,10 +17,50 @@ const createElement = (tag, className) => {
   element.className = className
   return element
 }
-const revealCard = ({ target }) => {
-  target.parentNode.classList.add("reveal-card")
-}
 
+let firstCard = ""
+let secondCard = ""
+
+const checkCards = () => {
+  const firstCharacter = firstCard.getAttribute("data-character")
+  const secondCharacter = secondCard.getAttribute("data-character")
+
+  if (firstCharacter == secondCharacter) {
+    firstCard.firstChild.classList.add("disabled-card")
+    secondCard.firstChild.classList.add("disabled-card")
+
+    firstCard = ""
+    secondCard = ""
+
+    checkEndGame()
+  } else {
+    setTimeout(() => {
+      firstCard.classList.remove("reveal-card")
+      secondCard.classList.remove("reveal-card")
+
+      firstCard = ""
+      secondCard = ""
+    }, 500)
+  }
+}
+const revealCard = ({ target }) => {
+  if (target.parentNode.className.includes("reveal-card")) {
+    return
+  }
+
+  if (firstCard == "") {
+
+    target.parentNode.classList.add("reveal-card")
+    firstCard = target.parentNode
+
+  } else if (secondCard == "") {
+
+    target.parentNode.classList.add("reveal-card")
+    secondCard = target.parentNode
+
+    checkCards()
+  }
+}
 const createCard = (character) => {
   const card = createElement("div", "card")
   const front = createElement("div", "face front")
@@ -47,5 +87,3 @@ const loadGame = () => {
   })
 }
 loadGame()
-
-//PAROU EM 39:58
