012ee8d96cb7da9e87ee41f2736b29a2f60033d7
florianopolismarcelo <florianopolismarcelo@msn.com>
2023-11-25 12:14:02 -0300

    criando combinacao de vitoria

--- jogo-da-velha01/script.js
+++ jogo-da-velha01/script.js
@@ -3,6 +3,18 @@ const board = document.querySelector("[data-board]")
 
 let isCircleTurn;
 
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+   
+]
+
 const startGame = () => {
     for (const cell of cellElements) {
         cell.addEventListener("click", handleClick, { once: true });
