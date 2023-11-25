1bc4451ecfd1e60bb9bb972729c77dd653c12041
florianopolismarcelo <florianopolismarcelo@msn.com>
2023-11-25 05:43:50 -0300

    construindo handleClick no javaScript

--- jogo-da-velha01/script.js
+++ jogo-da-velha01/script.js
@@ -1,13 +1,18 @@
 const cellElements = document.querySelectorAll("[data-cell]")
 
-for (const cell of cellElements) {
-    cell.addEventListener ("click", handleClick, { once: true });
-}
+let isCircleTurn;
 
-const handleClick = () => {
+const handleClick = (e) => {
 // Colocar a marca do (X ou Circulo)
+const cell = e.target;
+const ClassToAdd = isCircleTurn ? "circle" : "x";
+
+cell.classList.add(ClassToAdd);
 // Verificar por vitórias
 // Verificar por empates
 // Mudar simbolo
 
+}
+for (const cell of cellElements) {
+    cell.addEventListener ("click", handleClick, { once: true });
 }
