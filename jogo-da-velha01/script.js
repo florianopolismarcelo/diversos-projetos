79a5088dd7f1d537bae68d6944a4b4c099ad4738
florianopolismarcelo <florianopolismarcelo@msn.com>
2023-11-26 07:31:05 -0300

    atualizando o que foi feito

--- jogo-da-velha01/script.js
+++ jogo-da-velha01/script.js
@@ -2,6 +2,7 @@ const cellElements = document.querySelectorAll("[data-cell]")
 const board = document.querySelector("[data-board]")
 const winningMessageTextElement = document.querySelector("[data-winning-message-text]")
 const winningMessage = document.querySelector("[data-winning-message]")
+const winningMessageButton = document.querySelector("[data-winning-message-button]")
 
 
 let isCircleTurn;
