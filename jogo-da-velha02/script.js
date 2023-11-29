a563f2cba098c08785b59b9da4a7043c9a852313
florianopolismarcelo <florianopolismarcelo@msn.com>
2023-11-29 06:05:15 -0300

    jogo da velha

--- jogo-da-velha02/script.js
+++ jogo-da-velha02/script.js
@@ -13,6 +13,63 @@ function fieldClick(id) {
     else localStorage.setItem("turn", "X")
     game = game.substring(0, Number(id) - 1) + turn + game.substring(Number(id))
     localStorage.setItem("game", game)
-    verifyWinner(game)
+    var winner = verifyWinner(game)
+    
+      if (winner)
+        setTimeout(function () {
+          window.alert(`${winner} Venceu!`)
+        }, 100)
   }
+}
+
+function verifyWinner(game){
+  var winner;
+  winner = verifyRow(game)
+
+  if (!winner) {
+  winner = verifyCol(game)
+  if (!winner) winner = verifyDiag(game)
+  }
+ return winner;
+}
+function verifyRow(game) {
+  if (game[0] != '-' && game[0] == game[1] && game[0] == game[2]) {
+    if (game[0] == 'X') return 'X'
+    else return 'O'
+  }
+  if (game[3] != "-" && game[3] == game[4] && game[3] == game[5]) {
+    if (game[3] == "X") return "X"
+    else return "O"
+  }
+  if (game[6] != "-" && game[6] == game[7] && game[6] == game[8]) {
+    if (game[6] == "X") return "X"
+    else return "O"
+  }
+  return;
+}
+function verifyCol(game) {
+    if (game[0] != "-" && game[0] == game[3] && game[0] == game[6]) {
+      if (game[0] == "X") return "X"
+      else return "O"
+    }
+    if (game[1] != "-" && game[1] == game[4] && game[1] == game[7]) {
+      if (game[1] == "X") return "X"
+      else return "O"
+    }
+    if (game[2] != "-" && game[2] == game[5] && game[2] == game[8]) {
+      if (game[2] == "X") return "X"
+      else return "O"
+    }
+    return;
+}
+function verifyDiag(game) {
+  if (game[0] != "-" && game[0] == game[4] && game[0] == game[8]) {
+      if (game[0] == "X") return "X"
+      else return "O"
+  }
+   if (game[2] != "-" && game[2] == game[4] && game[2] == game[6]) {
+      if (game[2] == "X") return "X"
+      else return "O"
+    }
+    return;
 }
