28c5263152afa3c90a62a6dd2d5921272bf5d580
florianopolismarcelo <florianopolismarcelo@msn.com>
2023-11-29 04:33:42 -0300

    jogo da velha

--- jogo-da-velha02/script.js
+++ jogo-da-velha02/script.js
@@ -1,5 +1,9 @@
 localStorage.setItem("turn", "X")
 
 function fieldClick(id) {
+  var turn = localStorage.getItem("turn")
   document.getElementById(id).innerHTML = localStorage.getItem("turn")
+
+  if (turn == "X") localStorage.setItem("turn", "O")
+  else localStorage.setItem("turn", "X")
 }
